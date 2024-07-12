3b4a39361edaef4baf0f5391a1cdbd0c5ba75e55
OSshiyanGuoGuoGuo <128673164+OSshiyanGuoGuoGuo@users.noreply.github.com>
2024-07-12 16:48:39 +0800

    Add files via upload
    api 1.2 before

--- Capi.java
+++ Capi.java
@@ -1,33 +1,48 @@
-public class Capi1 {
+public class Capi {
     private int value1;
     private int value2;
     private int value3;
-    // add parameter
-    public void doSomething(int param1, int param2, int param3) {
-        int result = calculateSum(param1, param2) + param3;
-        int doubledResult = getDoubleValue(result);
+
+    public void doSomething(int param1, int param2) {
+        int result = param1 + param2;
+        int doubledResult = result * 2;
         System.out.println("Result: " + doubledResult);
     }
-    // extract method
-    private int calculateSum(int a, int b) {
-        return a + b + value3;
-    }
-    // add method
-    private int getDoubleValue(int value) {
-        return value * 2;
-    }
-    // delete method: getTripleValue()
-    // delete parameter
-    public int calculateArea(int length) {
-        return length * length;
+
+
+    public void showValue(int param1, int param2) {
+        System.out.println(param1 + " " + param2);
     }
 
-    public int calculateDiscount(int originalPrice, int discountPercentage) {
-        int discountAmount = getDiscountAmount(originalPrice, discountPercentage);
-        return originalPrice - discountAmount;
+    public int getTripleValue(int value) {
+        return value * 3;
     }
-    // inline method
-    private int getDiscountAmount(int price, int percentage) {
-        return price * percentage / 100;
+
+
+    public int addV1V2(int value1, int value2) {
+        return value1 + value2;
     }
+
+    public int getDoubleValueOfSum(int value1, int value2) {
+        int sum = value1 + value2;
+        return sum * 2;
+    }
+
+
+
+
+
+
+    public int getSumOfValues() {
+        return value1 + value2 + value3;
+    }
+
+    public void printSumOfValues() {
+        System.out.println("Sum of values: " + getSumOfValues());
+    }
+
+    public int getTripleValue(int value) {
+        return value * 3;
+    }
+    
 }
