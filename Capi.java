046beddb350c38ac4f5d0343d094037fea88a546
OSshiyanGuoGuoGuo <128673164+OSshiyanGuoGuoGuo@users.noreply.github.com>
2024-07-12 11:47:38 +0800

    Add files via upload
    compare api before

--- Capi.java
+++ Capi.java
@@ -1,33 +1,29 @@
-public class Capi1 {
+public class Capi {
     private int value1;
     private int value2;
     private int value3;
-    // add parameter
-    public void doSomething(int param1, int param2, int param3) {
-        int result = calculateSum(param1, param2);
-        int doubledResult = getDoubleValue(result);
+
+    public void doSomething(int param1, int param2) {
+        int result = param1 + param2;
+        int doubledResult = result * 2;
         System.out.println("Result: " + doubledResult);
     }
-    // extract method
-    private int calculateSum(int a, int b) {
-        return a + b + value3;
+
+    public int getTripleValue(int value) {
+        return value * 3;
     }
-    // add method
-    private int getDoubleValue(int value) {
-        return value * 2;
-    }
-    // delete method: getTripleValue()
-    // delete parameter
-    public int calculateArea(int length) {
-        return length * length;
+
+
+
+
+
+
+    public int calculateArea(int length, int width) {
+        return length * width;
     }
 
     public int calculateDiscount(int originalPrice, int discountPercentage) {
-        int discountAmount = getDiscountAmount(originalPrice, discountPercentage);
+        int discountAmount = originalPrice * discountPercentage / 100;
         return originalPrice - discountAmount;
-    }
-    // inline method
-    private int getDiscountAmount(int price, int percentage) {
-        return price * percentage / 100;
     }
 }
