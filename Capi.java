6c80d6c0c24ff6c80d8fcb22bfd0984ab4206287
OSshiyanGuoGuoGuo <128673164+OSshiyanGuoGuoGuo@users.noreply.github.com>
2024-07-12 16:49:09 +0800

    Add files via upload
    api changed1.2 after

--- Capi.java
+++ Capi.java
@@ -1,48 +1,49 @@
-public class Capi {
+public class Capi1 {
     private int value1;
     private int value2;
     private int value3;
-
-    public void doSomething(int param1, int param2) {
-        int result = param1 + param2;
+    // add parameter
+    public void doSomething(int param1, int param2, int param3) {
+        int result = param1 + param2 + param3;
         int doubledResult = result * 2;
         System.out.println("Result: " + doubledResult);
     }
-
-
-    public void showValue(int param1, int param2) {
-        System.out.println(param1 + " " + param2);
+    
+    // delete parameter
+    public void showValue(int param1) {
+        System.out.println(param1);
     }
-
-    public int getTripleValue(int value) {
-        return value * 3;
+    
+    // add method
+    public void setValue1(int value1) {
+        this.value1 = value1;
     }
-
-
-    public int addV1V2(int value1, int value2) {
-        return value1 + value2;
+    
+    // delete method addV1V2
+    
+    
+    
+    // Extract Method
+    private int calculateDoubleValue(int value) {
+        return value * 2;
     }
 
     public int getDoubleValueOfSum(int value1, int value2) {
         int sum = value1 + value2;
-        return sum * 2;
+        return calculateDoubleValue(sum);
     }
-
-
-
-
-
-
-    public int getSumOfValues() {
-        return value1 + value2 + value3;
-    }
-
+    // Inline Method
     public void printSumOfValues() {
-        System.out.println("Sum of values: " + getSumOfValues());
+        System.out.println("Sum of values: " + (value1 + value2 + value3));
     }
 
-    public int getTripleValue(int value) {
+   
+   
+   
+    // Change method modifier from public to protected
+    protected int getTripleValue(int value) {
         return value * 3;
     }
+
     
 }
