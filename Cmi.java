b48006dc34d91c9aed0879d9e25814ee57bedba3
OSshiyanGuoGuoGuo <128673164+OSshiyanGuoGuoGuo@users.noreply.github.com>
2024-07-12 10:52:23 +0800

    Add files via upload
    compare method before

--- Cmi.java
+++ Cmi.java
@@ -1,26 +1,14 @@
-public class Cmi1 {
+public class Cmi {
     public static void main(String[] args) {
 
-        // 函数调用参数发生变更
-        BankAccount account1 = new BankAccount("12345678",9999.0);
+        BankAccount account1 = new BankAccount("12345678", 1000.0);
+        BankAccount account2 = new BankAccount("87654321", 2000.0);
 
-        //同名函数调用替换
-        BankAccount account2 = new BankAccount("87654321");
+        account1.deposit(500.0);
+        account1.withdraw(200.0);
+        account1.withdraw(2000.0);
 
-        // 函数调用base变量变更
-        account2.deposit(500.0);
-        account2.withdraw(200.0);
-
-        // 删除函数调用
-
-        // Display account balances
         System.out.println("account1 current balance: $" + account1.getBalance());
         System.out.println("account2 current balance: $" + account2.getBalance());
-
-        // 增加函数调用
-        System.out.println(account2.getBalance());
     }
 }
-
-
-
