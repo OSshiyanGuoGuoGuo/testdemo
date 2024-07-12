6a5efef5612ce8810235bf9b390cee56293c197f
OSshiyanGuoGuoGuo <128673164+OSshiyanGuoGuoGuo@users.noreply.github.com>
2024-07-12 09:05:24 +0800

    Add files via upload
    compare data flow before

---
+++ Cdf.java
@@ -0,0 +1,16 @@
+public class Cdf1 {
+    public void foo() {
+        int a, b, c, d, e;
+        a = 0;
+        b = 2;
+        c = Math.max(a,b);
+        d = 13;
+        System.out.println("Java");
+        System.out.println("Hello");
+        a = 1;
+        b = 1;
+        c = 1;
+        d = 1;
+        e = 1;
+    }
+}
